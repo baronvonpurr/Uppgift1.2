6c39ec4ccc73e9c326a30d09d8890dfbf90b91e9
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-06 10:57:37 +0200

    Add project files.

---
+++ Uppgift1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Your Sytem has encountered a fatal error");
+        }
+    }
+}
